64b044a538037bf2182bd95834e461b3ad4ed337
Nitin Malviya <nitinmalviya172@gmail.com>
2025-01-05 15:59:19 +0530

    add ejs config and page

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,45 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.config';
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.config";
+import path from "path";
+import { fileURLToPath } from "url";
+import expressLayouts from "express-ejs-layouts";
+import { clear, log } from "console";
+import e from "express";
+
+// Type declaration for import.meta
+declare global {
+  interface ImportMeta {
+    url: string;
+  }
+}
+clear;
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// EJS configuration
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Express layouts setup
+app.use(expressLayouts);
+app.set("layout", "layouts/main");
+
+// Static files setup - points to public folder at project root
+app.use(express.static(path.join(__dirname, "../public")));
+
+console.log('public -- path', path.join(__dirname, "../public"));
+
+
+log("import.meta.url", __dirname);
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(compression());
@@ -20,16 +49,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const server = http.createServer(app);
 
+app.get("/", (req, res) => {
+  res.render("pages/index", {
+    title: "Home Page", 
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("pages/about", {
+    title: "About Page", 
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("pages/contact", {
+    title: "Contact Page", 
+  });
+});
+
+app.get("/test-backend", (req, res) => {
+  res.send("Hello Computer-villa!");
+});
+
 const startServer = async () => {
-    try {
-        await connectDB();
-        server.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.error('Server startup failed:', error);
-        process.exit(1);
-    }
+  try {
+    await connectDB();
+    server.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Server startup failed:", error);
+    process.exit(1);
+  }
 };
 
 startServer();
